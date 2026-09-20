654a59ffb7989e2d53c7c2e230975d200f370620
chlimaverde3 <ch.limaverde3@gmail.com>
2026-09-20 10:48:14 -0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "br.com.cifrasgospel"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "br.com.cifrasgospel"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1"
+    }
+}
